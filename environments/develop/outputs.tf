49de73225d04b9ad16fba1dce5fc3090dec4d2b8
julianloaiza <loaizajulian1999@gmail.com>
2025-03-27 17:32:47 -0500

    Remove sensitive AWS access key output from development environment for security compliance

--- environments/develop/outputs.tf
+++ environments/develop/outputs.tf
@@ -14,10 +14,3 @@ output "project" {
   description = "The project name"
   value       = var.project
 }
-
-# No mostramos las credenciales en los outputs por seguridad
-output "aws_access_key_used" {
-  description = "Confirmation that AWS access key is being used"
-  value       = var.aws_access_key != null ? "Access key configured" : "No access key provided"
-  sensitive   = true
-}
